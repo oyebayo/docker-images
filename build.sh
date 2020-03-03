5c806c737fda4a02f1344a852d362778fa56fc8f
Bayo Oyekole <oyebayo.oyekole@talabat.com>
2020-03-04 01:33:25 +0400

    Added build command

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t oyekoleb/sdk-3.1-nfv-ci-tools:1.1 -f sdk-3.1-nfv-ci-tools/Dockerfile .
+docker build -t oyekoleb/sdk-3.1-pytest-psql:1.7 -f sdk-3.1-pytest-psql/Dockerfile .
+docker build -t oyekoleb/docker-ce-git-awscli:1.2 -f docker-ce-git-awscli/Dockerfile .
