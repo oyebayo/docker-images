f5fde18b977a3cb02af2fd600fdc3d15b1206656
Bayo Oyekole <oyebayo.oyekole@talabat.com>
2020-03-06 03:51:27 +0400

    #fix for incorrect bump

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
 # get latest tag
+echo "Looking for file at $CIRCLE_WORKING_DIRECTORY/res/VERSION..."
 t=$(cat $CIRCLE_WORKING_DIRECTORY/res/VERSION)
-log=$(git log --pretty=oneline)
+echo "t = $t"
 
 # if there are none, start tags at 0.0.1
 if [ -z "$t" ]
 then
+    echo "Using default start..."
     t=0.0.1-b0
 fi
 
-# get commit logs and determine how to bump the version
-# supports #major, #minor, #patch (anything else will be 'build')
+# split $t using the hyphen as a separator (the D)
+# and take the 2nd field
+build_tag="$(cut -d'-' -f 2 <<< $t)"
+build_num="$(cut -d'b' -f 2 <<< $build_tag)"
+echo "build = $build_num"
+
+## get commit logs and determine how to bump the version
+## supports #major, #minor, #patch (anything else will be 'build')
+log=$(git log --pretty=oneline)
 case "$log" in
     *#major* ) new=$(semver.sh bump major $t);;
     *#feature* ) new=$(semver.sh bump minor $t);;
     *#fix* ) new=$(semver.sh bump patch $t);;
+    * ) new=$(semver.sh bump prerel b$((build_num+1)) $t);;
 esac
 
-# if the developer didnt annotate his commit, 'new' will be empty
-# so we default it to the start value: t
-if [ -z "$new" ]
-then
-    new=$t
-fi
-# always increment the build as per Circle CI
-new=$(semver.sh bump patch $new)
-new=$(semver.sh bump prerel b$CIRCLE_BUILD_NUM $new)
+echo "new  = $new..."
 echo $new > $CIRCLE_WORKING_DIRECTORY/res/VERSION
 
-# Let git know the version has been updated
-git add $CIRCLE_WORKING_DIRECTORY/res/VERSION
-git pull
-git commit -m "This is an automated commit by CircleCI [skip ci]"
-git push -u origin $CIRCLE_BRANCH
+## Let git know the version has been updated
+##git add $CIRCLE_WORKING_DIRECTORY/res/VERSION
+##git pull
+##git commit -m "This is an automated commit by CircleCI [skip ci]"
+##git push -u origin $CIRCLE_BRANCH
 
-echo "New version is $new"
+#echo "New version is $new"
