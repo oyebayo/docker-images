a232a9648d8977f0ebae01bd35395a883e183bcd
Bayo Oyekole <oyebayo.oyekole@talabat.com>
2020-03-06 04:58:34 +0400

    Consolidate build images into one

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-docker build -t oyekoleb/sdk-3.1-nfv-ci-tools:1.2 -f sdk-3.1-nfv-ci-tools/Dockerfile .
-docker build -t oyekoleb/sdk-3.1-pytest-psql:1.7 -f sdk-3.1-pytest-psql/Dockerfile .
-docker build -t oyekoleb/docker-ce-git-awscli:1.2 -f docker-ce-git-awscli/Dockerfile .
+#!/bin/bash
+tag=$1
+docker build -t oyekoleb/sdk-3.1-nfv-ci-tools:$tag -f sdk-3.1-nfv-ci-tools/Dockerfile .
+docker push oyekoleb/sdk-3.1-nfv-ci-tools:$tag
